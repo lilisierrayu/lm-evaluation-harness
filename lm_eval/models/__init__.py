29de1f402d149897919ee203ad6a9ec4b514bf63
Lili Yu <yulili8989@gmail.com>
2022-06-14 09:56:15 -0700

    initial code for adding cm3 api call

--- lm_eval/models/__init__.py
+++ lm_eval/models/__init__.py
@@ -1,11 +1,13 @@
 from . import gpt2
 from . import gpt3
 from . import dummy
+from . import cm3
 
 MODEL_REGISTRY = {
     "hf": gpt2.HFLM,
     "gpt2": gpt2.GPT2LM,
     "gpt3": gpt3.GPT3LM,
+    "cm3": cm3.CM3LM,
     "dummy": dummy.DummyLM,
 }
 
